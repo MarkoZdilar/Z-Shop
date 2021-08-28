91724658a82491be6c1e0e9b221326c58c11003b
MarkoZdilar <m.zdilar2011@gmail.com>
2021-08-28 18:47:56 +0200

    Code cleanup

--- ZShop/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Create.razor.g.cs
+++ ZShop/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Create.razor.g.cs
@@ -13,175 +13,175 @@ namespace ZShop.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 1 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 2 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 3 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 4 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 5 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 6 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 7 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 8 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 9 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 10 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 11 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 12 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Services.ProductService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 13 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Services.CategoryService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 14 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Services.CartService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 15 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Services.StatsService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 16 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Services.PlatformService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 17 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 17 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using ZShop.Client.Services.VariantService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 18 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 18 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Blazored.LocalStorage;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 19 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 19 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Blazored.Toast;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 20 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 20 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Blazored.Toast.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 21 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 21 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 22 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 22 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Blazored.Typeahead;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 23 "D:\Projects\Z-Shop\ZShop\Client\_Imports.razor"
+#line 23 "D:\Z-Shop\Z-Shop23\ZShop\Client\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Projects\Z-Shop\ZShop\Client\Pages\Create.razor"
+#line 2 "D:\Z-Shop\Z-Shop23\ZShop\Client\Pages\Create.razor"
 using ZShop.Shared.ProductModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Projects\Z-Shop\ZShop\Client\Pages\Create.razor"
+#line 9 "D:\Z-Shop\Z-Shop23\ZShop\Client\Pages\Create.razor"
            [Authorize(Roles = "Admin")]
 
 #line default
@@ -196,9 +196,9 @@ using ZShop.Shared.ProductModels;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 44 "D:\Projects\Z-Shop\ZShop\Client\Pages\Create.razor"
+#line 44 "D:\Z-Shop\Z-Shop23\ZShop\Client\Pages\Create.razor"
        
-    private ProductEditModel newProduct = new ProductEditModel();
+    private ProductAddModel newProduct = new ProductAddModel();
 
     private async Task HandleValidSubmit()
     {
